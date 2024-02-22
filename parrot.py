b7f25839d548df50555d4c171d734cb237f79fc2
imdylanpro <159092190+imdylanpro@users.noreply.github.com>
2024-02-21 17:48:33 -0800

    Create parrot.py
    acquires user input then repeats it back to the user.

---
+++ parrot.py
@@ -0,0 +1,9 @@
+# Dylan Nelson
+# February 21, 2024
+# parrot.py
+
+# This program will get a user's input and speak it back to them
+
+message = input("Tell me something, and I will repeat it back to you: ")
+
+print(message)
